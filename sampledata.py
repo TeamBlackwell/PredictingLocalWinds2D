dc3cd8f5c3f25168c47368aa555090428c667e8a
ThisAbhinav <this.abhinav26@gmail.com>
2024-09-29 23:29:49 +0530

    feat: basic MLP and data loader

--- sampledata.py
+++ sampledata.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 # sample_data = np.array([((0,0), (30,10)), ((50,40), (10,40)), ((0,40),(2,12)),((50,40), (10,40)),((50,0), (10,4))])
-sample_data = np.random.randint(0, 100, size=(4, 3, 2))
+sample_data = np.random.randint(0, 100, size=(100, 100, 2))
 print(sample_data)
-np.save('./data/wind_fields/2_m.npy', sample_data)
+np.save('./data/wind_fields/3_m.npy', sample_data)
