13df8ca5bb31c241a18d6b03730872378606c4df
PjrCodes <25995849+PjrCodes@users.noreply.github.com>
2024-10-10 14:07:38 +0530

    formatting, requiremetns

--- model.py
+++ model.py
@@ -1,22 +1,27 @@
 import torch
 import torch.nn as nn
 
+
 class BasicMLP(nn.Module):
     def __init__(self, local):
         super(BasicMLP, self).__init__()
         input_size = 360 + 2  # 360-dimensional vector + 2 for wind at robot's location
-        output_size = (((local * 2) + 1) ** 2) * 2  # Adjusted for the additional dimension
+        output_size = (
+            ((local * 2) + 1) ** 2
+        ) * 2  # Adjusted for the additional dimension
         self.fc1 = nn.Linear(input_size, 256)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, output_size)
         self.relu = nn.ReLU()
         self.local = local
-        
+
     def forward(self, x):
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
         x = self.relu(x)
         x = self.fc3(x)
-        x = x.view(-1, self.local * 2 + 1, self.local * 2 + 1, 2)  # Reshape to the desired output size
+        x = x.view(
+            -1, self.local * 2 + 1, self.local * 2 + 1, 2
+        )  # Reshape to the desired output size
         return x
